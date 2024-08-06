b830e994aaaf2265c6fb9bf3c8e51c3d75531b1a
PARTHIB DEB <parthibkumardeb@gmail.com>
2024-08-07 01:44:50 +0530

    add : NewChat Model

--- Backend/chat/models.py
+++ Backend/chat/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from users.models import *
+import datetime
 
 # Create your models here.
+
+class NewChat(models.Model):
+    sender_username = models.CharField(max_length=150)
+    receiver_username = models.CharField(max_length=150)
+    Date = models.DateField(default=datetime.date.today)
+    start_time = models.TimeField(default=datetime.datetime.now().time)
+    message = models.CharField()
+    
+    def __str__(self) -> str:
+        return f"Sender:{self.sender_username} , Receiver:{self.receiver_username}"
