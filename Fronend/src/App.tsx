fa45743b091bd03b5f1fc77ed94b83f5a76b15ce
PARTHIB DEB <parthibkumardeb@gmail.com>
2024-08-06 12:22:44 +0530

    add : Frontend Architechture (React,TS,Vite,Tailwind)

---
+++ Fronend/src/App.tsx
@@ -0,0 +1,12 @@
+// import { useState } from 'react'
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <h1 className='text-5xl'>React Django Chat-App</h1>
+  )
+}
+
+export default App
