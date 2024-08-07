ad3eed6b7041fcf12049ce92597a4e4719cc84f4
PARTHIB DEB <parthibkumardeb@gmail.com>
2024-08-07 23:52:34 +0530

    add : url of websocket-consumer(ChatConsumer)

--- Backend/api/urls.py
+++ Backend/api/urls.py
@@ -1,9 +1,13 @@
-from django.urls import path,include
+from django.urls import path,include,re_path
 from users.views import *
 from chat.views import *
+from chat.consumers import *
 
 urlpatterns = [
     path("register",UserView.as_view(),name="Register-User"),
-    # path("login",)
-    # path("chats")
+    path('auth', include('rest_framework.urls', namespace='rest_framework')) # auth/login -> login , auth/logout -> logout
+]
+
+websocket_urlpatterns = [
+    re_path(r"ws/chat", ChatConsumer.as_asgi()),
 ]
