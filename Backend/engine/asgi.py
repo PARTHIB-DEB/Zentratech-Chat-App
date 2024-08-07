964a792d6897f7947cbe60b9f0b09f49389aa6a4
PARTHIB DEB <parthibkumardeb@gmail.com>
2024-08-08 01:03:12 +0530

    add : Auth layer on websocket url

--- Backend/engine/asgi.py
+++ Backend/engine/asgi.py
@@ -10,21 +10,19 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter,URLRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from channels.security.websocket import AllowedHostsOriginValidator
 from api.urls import websocket_urlpatterns
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'engine.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "engine.settings")
 
 
-application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket":AllowedHostsOriginValidator(
-        AuthMiddlewareStack(
-        URLRouter(
-            websocket_urlpatterns
-        )
-    )
-    )
-})
+application = ProtocolTypeRouter(
+    {
+        "http": get_asgi_application(),
+        "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
+        ),
+    }
+)
