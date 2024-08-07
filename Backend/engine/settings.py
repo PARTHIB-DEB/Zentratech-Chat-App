c6b385d1f3d4dfde7bd126d90ff4d6587f497705
PARTHIB DEB <parthibkumardeb@gmail.com>
2024-08-08 00:36:01 +0530

    add : authmiddleware and websocket-Url

--- Backend/engine/settings.py
+++ Backend/engine/settings.py
@@ -11,9 +11,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 # Caution !!
-'''
+"""
 will have to run `daphne --root-path=/forum django_project.asgi:application` after complete models and views setup of both apps
-'''
+"""
 
 from pathlib import Path
 
@@ -25,40 +25,40 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5w^(79!78(reo7j&=o@5852fd#0@63fka!8^0k$!4$4lf@=fi0'
+SECRET_KEY = "django-insecure-5w^(79!78(reo7j&=o@5852fd#0@63fka!8^0k$!4$4lf@=fi0"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'chat', # Django App
-    'users', # Django App
-    'corsheaders', # CORS
-    'rest_framework', # DRF
-    'daphne',
-    'channels'
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "chat",  # Django App
+    "users",  # Django App
+    "corsheaders",  # CORS
+    "rest_framework",  # DRF
+    "daphne",
+    "channels",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware' # CORS
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",  # CORS
 ]
 
 CORS_ALLOWED_ORIGINS = [
@@ -87,44 +87,39 @@ CORS_ALLOW_HEADERS = (
     "x-requested-with",
 )
 
-ROOT_URLCONF = 'engine.urls'
+ROOT_URLCONF = "engine.urls"
 
-AUTH_USER_MODEL = 'users.newuser'
+AUTH_USER_MODEL = "users.newuser"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'engine.wsgi.application'
-ASGI_APPLICATION = 'engine.asgi.application' # For creating a server based on ASGI rules
+WSGI_APPLICATION = "engine.wsgi.application"
+ASGI_APPLICATION = (
+    "engine.asgi.application"  # For creating a server based on ASGI rules
+)
 
-CHANNEL_LAYERS = {
-    "default": {
-        "BACKEND": "channels.layers.InMemoryChannelLayer"
-    }
-}
+CHANNEL_LAYERS = {"default": {"BACKEND": "channels.layers.InMemoryChannelLayer"}}
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES':[
-        'rest_framework.permissions.IsAuthenticated'
-    ]
-    ,
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
-    ]
+    "DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.IsAuthenticated"],
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.BasicAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+    ],
 }
 
 # Database
@@ -136,16 +131,16 @@ REST_FRAMEWORK = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -153,9 +148,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -165,9 +160,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
--- Backend/users/views.py
+++ Backend/users/views.py
@@ -7,53 +7,54 @@ from rest_framework.authentication import SessionAuthentication, BasicAuthentica
 from rest_framework.permissions import IsAuthenticated
 # Create your views here.
 
+
 class UserView(APIView):
     authentication_classes = [SessionAuthentication, BasicAuthentication]
     permission_classes = [IsAuthenticated]
-    def get(self,request):
-        return Response({
-            "User":"str(request.user)",
-            "Auth":"str(request.auth)"
-        })
-    
-    def post(self,request):
-        user_datas=UserSerializer(data=request.data)
+
+    def get(self, request):
+        return Response({"User": "str(request.user)", "Auth": "str(request.auth)"})
+
+    def post(self, request):
+        user_datas = UserSerializer(data=request.data)
         if user_datas.is_valid():
             user_datas.save()
             uname = user_datas.validated_data.get("username")
-            return Response({
-                "Message":"Successfully Registered",
-                "Logged-In":f"{NewUser.objects.get(username=uname).is_authenticated}"
-            })        
+            return Response(
+                {
+                    "Message": "Successfully Registered",
+                    "Logged-In": f"{NewUser.objects.get(username=uname).is_authenticated}",
+                }
+            )
         else:
             return Response(user_datas.errors)
-        
-    def put(self,request):
-        data=request.data
-        update_user=data['username']
-        user_datas=UserSerializer(data=data)
+
+    def put(self, request):
+        data = request.data
+        update_user = data["username"]
+        user_datas = UserSerializer(data=data)
         if user_datas.is_valid():
             user_datas.save()
-            return Response({"Message":f"User {update_user} is updated completely"})
+            return Response({"Message": f"User {update_user} is updated completely"})
         else:
             return Response(user_datas.errors)
-        
-    def patch(self,request):
-        data=request.data
-        update_user=data['username']
-        user_datas=UserSerializer(data=data,partial=True)
+
+    def patch(self, request):
+        data = request.data
+        update_user = data["username"]
+        user_datas = UserSerializer(data=data, partial=True)
         if user_datas.is_valid():
             user_datas.save()
-            return Response({"Message":f"User {update_user} is updated"})
+            return Response({"Message": f"User {update_user} is updated"})
         else:
             return Response(user_datas.errors)
-    
-    def delete(self,request):
-        data=request.data
-        delete_user=data['username']
-        if len(delete_user)>=5:
+
+    def delete(self, request):
+        data = request.data
+        delete_user = data["username"]
+        if len(delete_user) >= 5:
             obj = NewUser.objects.get(username=delete_user)
             obj.delete()
-            return Response({"Message":f"User {obj.username} is deleted"})
+            return Response({"Message": f"User {obj.username} is deleted"})
         else:
             raise NotFound(f"User of Username {delete_user} doesn't exists")
