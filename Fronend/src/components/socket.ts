9b074d14c20c1d3f7594c3fde728d2731eb2ee69
PARTHIB DEB <parthibkumardeb@gmail.com>
2024-08-09 16:26:39 +0530

    add : logic for websocket messages

--- Fronend/src/components/socket.ts
+++ Fronend/src/components/socket.ts
@@ -1,4 +1,43 @@
-export default function getSenderUsername() :string {
-    let username:string = "";
-    return username
+import { io, Socket } from "socket.io-client";
+
+interface SendMessageFormat {
+  sender_username: string;
+  sender_message: string;
 }
+
+interface ReceiveMessageFormat {
+  type: string;
+  sender_username: string;
+  receiver_username: string;
+  sender_message: string;
+  receiver_message: string;
+}
+
+let socket: Socket | null = null;
+
+export const connect = (url: string): void => {
+    socket = io(url);
+    console.log("Socket connected.");
+};
+
+export function chat (sendMessageData?: SendMessageFormat, onReceive?: (messageData: ReceiveMessageFormat) => void): void {
+    if (!socket) return;
+
+    if (sendMessageData) {
+        socket.emit("chat", sendMessageData);
+    }
+
+    if (onReceive) {
+        socket.on("chat_message", (msg: ReceiveMessageFormat) => {
+        console.log("Message received:", msg);
+        onReceive(msg);
+        });
+    }
+};
+
+export const disconnect = (): void => {
+    if (socket) {
+        socket.disconnect();
+        console.log("Socket disconnected.");
+    }
+};
