1145c778a643f8e1a5c70ff2f10677f2a22beb8b
PARTHIB DEB <parthibkumardeb@gmail.com>
2024-08-09 16:25:50 +0530

    rm : {username} field removed

--- Fronend/src/components/login.tsx
+++ Fronend/src/components/login.tsx
@@ -1,24 +1,20 @@
-import username from "./socket"
-
 interface input_props{
     width:string,
     padding:string,
     small_text_weight:string,
     large_text_weight:string,
-    username : string
 }
 let input_obj : input_props={
     width : "w-[90%]",
     padding : "p-[0.2rem]",
     small_text_weight : "text-base",
-    large_text_weight : "md:text-lg",
-    username : `${username}`
+    large_text_weight : "md:text-lg"
 }
 export default function Login (){
     const props_stack = `${input_obj.width}${input_obj.padding}${input_obj.small_text_weight}${input_obj.large_text_weight}`
   return (
     <div className="user-form h-1/2 w-1/2 font-normal rounded-lg flex flex-col justify-evenly items-center bg-white shadow-purple-400 shadow-md">
-        <p className="text-center flex justify-center text-2xl md:text-4xl underline">Login{" "}{input_obj.username}</p>
+        <p className="text-center flex justify-center text-2xl md:text-4xl underline">Login User</p>
         <div className="username flex justify-center items-center">
             <label htmlFor="username" className="text-xl md:text-2xl">Username</label>
             <input type="text" name="username" id="username" className={props_stack} placeholder="Enter Your Username"/>
