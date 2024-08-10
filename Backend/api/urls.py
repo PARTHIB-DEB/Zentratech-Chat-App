0d7b03c352827007e5e5ec06121f46b0dc331dc9
PARTHIB DEB <parthibkumardeb@gmail.com>
2024-08-10 15:46:55 +0530

    auth/login path-func rearranged

--- Backend/api/urls.py
+++ Backend/api/urls.py
@@ -4,11 +4,9 @@ from chat.views import *
 from chat.consumers import *
 
 urlpatterns = [
+    path("auth", include("rest_framework.urls", namespace="rest_framework")),  # auth/login -> login , auth/logout -> logout
     path("register", UserView.as_view(), name="Register-User"),
     path("request",Requestview.as_view(),name="Friend-Request"),
-    path(
-        "auth", include("rest_framework.urls", namespace="rest_framework")
-    ),  # auth/login -> login , auth/logout -> logout
 ]
 
 websocket_urlpatterns = [
