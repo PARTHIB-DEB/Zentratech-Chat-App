53fe64d481605bbc4ce22144fff303be04c43aee
PARTHIB DEB <parthibkumardeb@gmail.com>
2024-08-10 15:58:15 +0530

    rm : socket disconnect logic

--- Fronend/src/components/socketchats.tsx
+++ Fronend/src/components/socketchats.tsx
@@ -21,9 +21,7 @@ export default function Socketchats() {
         setChatLog((prevChat) => [...prevChat, msg]);
       });
 
-      return () => {
-        disconnect();
-      };
+
   }, []);
 
   const handleSendMessage = () => {
@@ -36,20 +34,24 @@ export default function Socketchats() {
   };
 
   return (
-    <div className="chatbox h-[90%] w-[80%] bg-white rounded-lg shadow-md shadow-blue-500 overflow-x-hidden pt-[1rem] flex justify-center items-center">
-        <div className="w-[90%] h-[40rem] gap-y-4">
-          {
-              chatLog.map((msg)=>(
-                <>
-                  <p className='h-1/4 w-full text-base font-bold text-black'>{msg.sender_username}</p>
-                  <div className="msg h-[8rem] text-base bg-slate-400 text-black">{msg.sender_message}</div>
-                  <p className='h-1/4 w-full text-base font-bold text-black'>{msg.receiver_username}</p>
-                  <div className="msg h-[8rem] text-base bg-slate-400 text-black">{msg.receiver_message}</div>
-                </>
-              ))
-          }
-          <input type="text" value={message} onChange={(e) => setMessage(e.target.value)} className='h-[12rem] w-full bg-slate-400' placeholder='write your message'/>
-          <button onClick={handleSendMessage} className='h-[4rem] w-[6rem] text-center bg-green-800 text-white text-lg'>Send Message</button>
+    <div className="chatbox h-[30rem] w-[60rem] bg-white rounded-lg overflow-x-hidden pt-[1rem] flex justify-center items-center flex-col">
+      <p className="text-center flex justify-center mb-4 text-2xl md:text-4xl underline">Chat Box</p>
+        <div className="w-[90%] h-[70%] gap-y-4 flex flex-col justify-evenly items-center">
+            <div className='flex flex-col justify-evenly items-center h-full w-[70%] bg-yellow-400'>
+              {
+                  chatLog.map((msg)=>(
+                    <><div className="b1">
+                      <p className='h-1/4 w-full text-base font-bold text-black'>{msg.sender_username}</p>
+                      <div className="msg h-[8rem] text-base bg-slate-400 text-black">{msg.sender_message}</div>
+                    </div><div className="b2">
+                        <p className='h-1/4 w-full text-base font-bold text-black'>{msg.receiver_username}</p>
+                        <div className="msg h-[8rem] text-base bg-slate-400 text-black">{msg.receiver_message}</div>
+                      </div></>
+                  ))
+              }
+            </div>
+          <input type="text" value={message} onChange={(e) => setMessage(e.target.value)} className='h-[6rem] w-full bg-slate-400  px-[1rem] rounded-lg' placeholder='write your message'/>
+          <button onClick={handleSendMessage} className='h-[3.5rem] w-[9rem] text-center bg-green-800 text-white text-lg rounded-lg'>Send Message</button>
         </div>
     </div>
   );
